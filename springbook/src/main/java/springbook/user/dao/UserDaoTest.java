2e7262c38a53ee456679335dacfa9b2b7ca0b3bb
LeeSeongKyong <llee_ss@naver.com>
2021-12-07 15:16:35 +0900

    update

--- springbook/src/main/java/springbook/user/dao/UserDaoTest.java
+++ springbook/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -10,6 +10,7 @@ public class UserDaoTest {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
+
 		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 
 		UserDao dao = context.getBean("userDao", UserDao.class);
